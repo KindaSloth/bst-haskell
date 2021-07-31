36fec9e612fc0ce4d35bd720fcfcf459c93576a5
Guilherme775 <guilhermereis28122002@gmail.com>
2021-07-30 22:01:27 -0300

    feat: add delete function

--- binary_search_tree.hs
+++ binary_search_tree.hs
@@ -15,3 +15,19 @@ search (Node left value right) x
     | x < value = search left x
     | x == value = (Node left value right)
     | otherwise = error "this numder don't exist in the tree yet"
+
+delete :: BST -> Int -> BST
+delete Null _ = Null
+delete (Node left value right) x
+    | x > value = Node left value (delete right x)
+    | x < value = Node (delete left x) value right
+    | x == value = deleteNode (Node left value right)
+    | otherwise = error "number already exist in the tree"
+
+deleteNode :: BST -> BST
+deleteNode (Node Null value right) = right
+deleteNode (Node left value Null) = left
+deleteNode (Node left value right) = insert left (getValue right)
+
+getValue :: BST -> Int
+getValue (Node _ value _) = value
